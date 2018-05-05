a16619ca14a149adf74dac0cb4c775e0cfcad825
Travis Williams <travis.williams63@gmail.com>
2018-05-05 11:21:20 -0400

    Initial Crystal Commit
    Initial Commit of the Crystal Project

---
+++ src/hellinacell.cr
@@ -0,0 +1,6 @@
+require "./hellinacell/*"
+
+# TODO: Write documentation for `Hellinacell`
+module Hellinacell
+  # TODO: Put your code here
+end
---
+++ src/hellinacell/version.cr
@@ -0,0 +1,3 @@
+module Hellinacell
+  VERSION = "0.1.0"
+end
